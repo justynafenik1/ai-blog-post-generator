ad4c7145fa3143d2be0cfa50d81a8be46030ab15
justynafenik1 <145045383+justynafenik1@users.noreply.github.com>
2025-08-27 08:18:39 +0200

    feat: add delete tests

---
+++ tests/specs/e2e/delete-post.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/fixtures";
+
+test.describe("Deleting post", () => {
+  /**
+   * Runs before each test to ensure the blog page is fully loaded and generate test post
+   */
+  let keyword = "letter";
+
+  test.beforeEach(async ({ blogPage, postGeneration }) => {
+    await blogPage.pageIsLoaded();
+    await postGeneration.generatePostWithOptionalTags(keyword);
+    await postGeneration.waitForPostLoaded();
+    await postGeneration.expectPostAddedToastVisible();
+  });
+  
+  /**
+   * This test verifies that a post with a given keyword can be deleted successfully.
+   */
+  test("TC-E2E-09: Delete post successfully", async ({
+    postList,
+    postDelete,
+  }) => {
+    await postDelete.clickDeletePost(keyword);
+    await postDelete.confirmDeleteOnPopup();
+    await postDelete.expectPostDeletedToastVisible();
+
+    const data = await postList.getPostsDataByKeyword(keyword);
+    expect(data.length).toBe(0);
+  });
+
+  /**
+   * This test checks that cancelling the delete action does not remove the post.
+   */
+  test("TC-E2E-10: Cancel deletion", async ({ postList, postDelete }) => {
+    await postDelete.clickDeletePost(keyword);
+    await postDelete.cancelDeleteOnPopup();
+    await postList.verifyAddedPostData(keyword, []);
+  });
+});
